8108ffd80b660b8a5ac4a6b8420f78cb23c84de1
Penaz <penazarea@altervista.org>
2020-01-24 12:00:24 +0100

    Added user choice for grid size

--- Random dungeon gen 6.py
+++ Random dungeon gen 6.py
@@ -2,31 +2,35 @@ import random
 import turtle
 from turtle import *
 
-#I attempted to implement user choice  of grid size but failed. If you have an idea as to how I can do that please feel free to message me!
-#gridlen = int(input("How big do you want the grid to be?"))
-
-
-
 #used for resets:
 another = 0
 yn = "Y"
-while yn == "Y":
+while yn.upper() == "Y":
     #setup of variables and functions
-    gridlen = 7
-    line1 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    line2 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    line3 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    line4 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    line5 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    line6 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    line7 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    grid = [line1,line2,line3,line4,line5,line6,line7]
+    grid_size_valid = False
+    # Since the algorithm needs to work out the "middle line", the size must be
+    # an odd number
+    # -------------------------
+    while not grid_size_valid:
+        gridlen = int(input("How big do you want the grid to be?"))
+        grid_size_valid = gridlen % 2 == 1
+        if not grid_size_valid:
+            print("The size of the grid must be an odd number")
+    # -------------------------
+    # This list comprehension creates a "gridlen by gridlen"
+    # (like 7x7, 9x9, ...) grid filled with [0,0,0,0]
+    grid = [
+        [
+            [0,0,0,0]
+            for _ in range(gridlen)
+        ]
+        for _ in range(gridlen)]
     thrd = 600/gridlen/3
   #assignig random 0's and 1's into the grid
     def assignrandom():
         for z in range(gridlen):
             for x in range(gridlen):
-                for y in range(len(line1[x])):
+                for y in range(len(grid[0][x])):
                   grid[z][x][y] = random.choice([1,0])
 
   #correcting the 1's and 0's to make sure paths are joining right to left
@@ -50,9 +54,9 @@ while yn == "Y":
     def outeredges():
         for x in range(len(grid)):
             grid[0][x][1] = 0
-            grid[6][x][3] = 0
+            grid[gridlen - 1][x][3] = 0
             grid[x][0][0] = 0
-            grid[x][6][2] = 0
+            grid[x][gridlen - 1][2] = 0
 
     #line1 = [[0,0,0,0] for _ in range(gridlen)] #duplicates [0,0,0,0] a number of times
     #grid = [line1.copy() for _ in range(gridlen)]
@@ -75,9 +79,9 @@ while yn == "Y":
  #animation of the turtle on / off
     anim = input("Do you want animation? Y/N?")
     #setup of turtle
-    if anim == "N":
+    if anim.upper() == "N":
         turtle.tracer(False)
-    elif anim == "Y":
+    elif anim.upper() == "Y":
         print("OK!")
         speed(100)
     if another == 1:
@@ -94,8 +98,8 @@ while yn == "Y":
         right(90)
         another = 1
     #code for path drawing
-    for x in range(7):
-        for y in range(7):
+    for x in range(gridlen):
+        for y in range(gridlen):
             for g in range(4):
               if grid[x][y] == [0,0,0,0]:
                   penup()
@@ -192,7 +196,7 @@ while yn == "Y":
         right(90)
         forward(600)
         right(180)
-    if anim == "N":
+    if anim.upper() == "N":
         turtle.tracer(True)
 
     yn = input("another? Y/N: ")
